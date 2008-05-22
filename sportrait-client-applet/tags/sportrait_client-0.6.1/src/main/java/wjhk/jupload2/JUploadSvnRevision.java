63797d4d3d1baca6d51d78a52d1b892c63492ab7
alex <alex@bff75499-2b28-0410-8f74-d29e96b56331>
2008-05-22 20:45:59 +0000

    [maven-release-plugin]  copy for tag sportrait_client-0.6.1

---
+++ sportrait-client-applet/tags/sportrait_client-0.6.1/src/main/java/wjhk/jupload2/JUploadSvnRevision.java
@@ -0,0 +1,9 @@
+
+			// WARNING - This file is auto-generated
+			// ANY CHANGES WILL BE OVERWRITTEN BY THE BUILD-PROCESS!
+			package wjhk.jupload2;
+			class JUploadSvnRevision {
+				final static String REVISION = "308";
+				final static String LASTCHANGED = "07/27/2007 10:30 AM";
+			}
+		
