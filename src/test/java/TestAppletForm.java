be5b10d15f24f827794df9bf0428f94b3c349f36
Alexander Josef <22647518+alexander-josef@users.noreply.github.com>
2008-09-02 15:50:55 +0000

    refactorings .... does it work? checking in code that has been changed quite a while ago

---
+++ src/test/java/TestAppletForm.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: alexanderjosef
+ * Date: Jul 1, 2008
+ * Time: 8:16:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestAppletForm {
+}
